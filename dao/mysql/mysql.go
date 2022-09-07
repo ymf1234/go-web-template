155e3d2ad2a064f522237837ba4de298d7b26cfc
Alan <yumengfei1234@163.com>
2022-09-07 22:43:23 +0800

    初始化go-web模板

--- dao/mysql/mysql.go
+++ dao/mysql/mysql.go
@@ -1,11 +1,47 @@
 package mysql
 
 import (
+	"database/sql"
+	"fmt"
+	"github.com/spf13/viper"
+	"go.uber.org/zap"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"time"
 )
 
-func initDB() (err error) {
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+var db *gorm.DB
+var sqlDB *sql.DB
+
+func InitDB() (err error) {
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local&tls=true",
+		viper.GetString("mysql.user"),
+		viper.GetString("mysql.password"),
+		viper.GetString("mysql.host"),
+		viper.GetInt("mysql.port"),
+		viper.GetString("mysql.dbname"),
+	)
+	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		zap.L().Error("connect DB failed", zap.Error(err))
+		return
+	}
+
+	sqlDB, err = db.DB()
+
+	// SetMaxIdleConns 设置空闲连接池中连接的最大数量
+	sqlDB.SetMaxIdleConns(viper.GetInt("mysql.max_idle_conns"))
+
+	// SetMaxOpenConns 设置打开数据库连接的最大数量。
+	sqlDB.SetMaxOpenConns(viper.GetInt("mysql.max_open_conns"))
+
+	// SetConnMaxLifetime 设置了连接可复用的最大时间。
+	sqlDB.SetConnMaxLifetime(time.Hour)
+
 	return
+}
+
+func Close() {
+	_ = sqlDB.Close()
 }
--- go.mod
+++ go.mod
@@ -5,7 +5,10 @@ go 1.16
 require (
 	github.com/fsnotify/fsnotify v1.5.4
 	github.com/gin-gonic/gin v1.8.1
+	github.com/go-redis/redis v6.15.9+incompatible
 	github.com/natefinch/lumberjack v2.0.0+incompatible
+	github.com/onsi/ginkgo v1.16.5 // indirect
+	github.com/onsi/gomega v1.20.2 // indirect
 	github.com/spf13/viper v1.12.0
 	go.uber.org/zap v1.17.0
 	gopkg.in/natefinch/lumberjack.v2 v2.0.0 // indirect
