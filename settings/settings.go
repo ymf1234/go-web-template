60ace4ca22443c16fd95b9d269d1e2540b368411
Alan <yumengfei1234@163.com>
2022-09-07 23:58:05 +0800

    viper配置文件保存在结构体中

--- settings/settings.go
+++ settings/settings.go
@@ -6,6 +6,42 @@ import (
 	"github.com/spf13/viper"
 )
 
+type AppConfig struct {
+	Name         string `mapstructure:"name"`
+	Mode         string `mapstructure:"mode"`
+	Version      string `mapstructure:"version"`
+	Port         int    `mapstructure:"port"`
+	*LogConfig   `mapstructure:"log"`
+	*MySQLConfig `mapstructure:"mysql"`
+	*RedisConfig `mapstructure:"redis"`
+}
+
+type LogConfig struct {
+	Level      string `mapstructure:"level"`
+	Filename   string `mapstructure:"filename"`
+	MaxSize    string `mapstructure:"max_size"`
+	MaxAge     string `mapstructure:"max_age"`
+	MaxBackups int    `mapstructure:"max_backups"`
+}
+
+type MySQLConfig struct {
+	Host         string `mapstructure:"host"`
+	User         string `mapstructure:"user"`
+	Password     string `mapstructure:"password"`
+	DbName       string `mapstructure:"db_name"`
+	Port         int    `mapstructure:"port"`
+	MaxOpenConns int    `mapstructure:"max_open_conns"`
+	MaxIdleConns int    `mapstructure:"max_idle_conns"`
+}
+
+type RedisConfig struct {
+	Host     string `mapstructure:"host"`
+	Password string `mapstructure:"password"`
+	DB       string `mapstructure:"db"`
+	Port     int    `mapstructure:"port"`
+	PoolSize int    `mapstructure:"pool_size"`
+}
+
 func Init() (err error) {
 	viper.SetConfigName("config") // 配置文件名称(无扩展名)
 	viper.SetConfigType("yaml")   // 如果配置文件的名称中没有扩展名，则需要配置此项
